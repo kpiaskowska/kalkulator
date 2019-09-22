072a67a255ef9f0d783b9790bf56d2d335d01459
kpiaskowska <kpiaskowska98@gmail.com>
2019-09-22 15:34:33 +0200

    Introduce menu to application

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -10,6 +10,18 @@ namespace kalkulator
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+                int menuOpction = Convert.ToInt32(Console.ReadLine());
+                if (menuOpction == 0)
+                {
+                    break;
+
+                }
+            }
         }
     }
 }
